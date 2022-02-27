7b219e0ffd9251d1a84c6acd8fd35fa1c0e6b6ff
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-02-27 09:29:44 +0900

    added test code

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,9 @@ import { useCallback, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import PostCard from "../src/components/PostCard";
 import { setBlog } from "../store/modules/blog";
+import { setCategory } from "../store/modules/category";
 import { RootState } from "../store/modules";
+import Loader from "../src/components/common/Loader";
 import useAxios, { ApiOption } from "../src/hooks/useAxios";
 const item = {
   title: "제목",
@@ -24,13 +26,10 @@ const Home: NextPage = () => {
 
   useEffect(() => {
     setBlogInfo();
-
-    console.log(isLoading);
-    console.log(data);
-    console.log(error);
+    setCategorys();
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [isLoading]);
 
   const setBlogInfo = useCallback(() => {
     dispatch(
@@ -43,9 +42,47 @@ const Home: NextPage = () => {
     );
   }, []);
 
+  const setCategorys = useCallback(() => {
+    dispatch(
+      setCategory([
+        {
+          categoryNo: "0",
+          categoryName: "1 CATEGORY",
+          groupNo: "",
+          groupOrder: "",
+          depth: "",
+        },
+        {
+          categoryNo: "1",
+          categoryName: "2 카테고리",
+          groupNo: "",
+          groupOrder: "",
+          depth: "",
+        },
+        {
+          categoryNo: "2",
+          categoryName: "3 카테고리",
+          groupNo: "",
+          groupOrder: "",
+          depth: "",
+        },
+      ])
+    );
+  }, []);
+
   return (
     <div>
       <div className={"thumbnail"}></div>
+
+      <div>
+        <Loader isLoading={true}>
+          <span>내용</span>
+        </Loader>
+
+        <Loader isLoading={false}>
+          <span>내용</span>
+        </Loader>
+      </div>
 
       <div>
         <h1>TEST</h1>
