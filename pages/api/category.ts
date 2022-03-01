80c607b0c39a2c39f471a0f76251bf987e5a0b31
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-03-01 19:17:02 +0900

    added test api

---
+++ pages/api/category.ts
@@ -0,0 +1,47 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  res.status(200).json({
+    status: "Ok",
+    message: "Success",
+    item: {
+      categorys: [
+        {
+          categoryNo: "0",
+          categoryName: "First Category",
+          groupNo: "",
+          groupOrder: "",
+          depth: "",
+        },
+        {
+          categoryNo: "1",
+          categoryName: "두번째 카테고리",
+          groupNo: "",
+          groupOrder: "",
+          depth: "",
+        },
+        {
+          categoryNo: "2",
+          categoryName: "마지막 Category",
+          groupNo: "",
+          groupOrder: "",
+          depth: "",
+        },
+      ],
+      posts: [
+        {
+          boardNo: "",
+          categoryNo: "",
+          title: "",
+          hits: "",
+          registDate: "",
+          registId: "",
+        },
+      ],
+    },
+  });
+}
