2b36d358611049850957f50946bb8dcbf7699bd0
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-03-01 19:17:22 +0900

    added Apioption ts

---
+++ src/common/ApiOptions.ts
@@ -0,0 +1,23 @@
+export type HttpMethod = "GET" | "POST" | "PUT" | "DELETE";
+
+export type ApiOption = {
+  url: string;
+  method: HttpMethod;
+};
+
+const ApiOptions = {
+  getBlogInfo: {
+    url: "/blog",
+    method: "GET",
+  } as ApiOption,
+  getPostsByCategoryNo: {
+    url: "/category",
+    method: "GET",
+  } as ApiOption,
+  getPostInfo: {
+    url: "/posts",
+    method: "GET",
+  } as ApiOption,
+};
+
+export default ApiOptions;
