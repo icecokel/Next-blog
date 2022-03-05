8281184e9f21577181447bcf083ae9eae082cf2e
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-03-05 23:11:43 +0900

    modify mainpage and style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,38 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 
 const Home: NextPage = () => {
-  return <div>메인</div>;
+  return (
+    <div className="main-page">
+      <h1>Welcome Eucalyptus </h1>
+      <div>
+        Sign up and make your own blog.
+        <div>
+          <button>Sign Up</button>
+        </div>
+      </div>
+      <div>
+        Popular Blog.
+        <ul>
+          <Link href={"/testAdmin"}>
+            <a>
+              <li>Test Admin</li>
+            </a>
+          </Link>
+          <Link href={"/testAdmin"}>
+            <a>
+              <li>Test Admin</li>
+            </a>
+          </Link>
+          <Link href={"/testAdmin"}>
+            <a>
+              <li>Test Admin</li>
+            </a>
+          </Link>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
