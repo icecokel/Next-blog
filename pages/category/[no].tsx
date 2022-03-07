fdfc0f9840bc963c31d199726e3f396816f4209d
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-03-07 13:27:51 +0900

    added categort page feafure

--- pages/category/[no].tsx
+++ pages/category/[no].tsx
@@ -1,18 +1,41 @@
 import { useRouter } from "next/router";
 import Loader from "../../src/components/common/Loader";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/modules";
+import { useEffect, useState } from "react";
+import { Category } from "../../store/modules/category";
+import { Post } from "../../src/components/PostCard";
 
 const Category = () => {
   const router = useRouter();
   const categoryNo = router.query.no;
+  const category = useSelector((state: RootState) => state.category);
+  const [currentCategory, setCurrentCategory] = useState<Category>();
+  const [postList, setPostList] = useState<Array<Post>>([]);
+
+  useEffect(() => {
+    getCurrentCategory();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentCategory]);
+
+  const getCurrentCategory = () => {
+    const tempCurrentCategory = category.find(
+      (item) => item.categoryNo === categoryNo
+    );
+    setCurrentCategory(tempCurrentCategory);
+  };
 
   return (
     <div>
-      <h2>Category Name {categoryNo}</h2>
+      <h2>{currentCategory?.categoryName}</h2>
       <hr />
       게시글 리스트
-      <Loader isLoading={true}>
+      <Loader isLoading={postList.length === 0}>
         <ul>
-          <li></li>
+          {postList.map((post, index) => {
+            return <li key={"post_" + index}>ㅇㅇ</li>;
+          })}
         </ul>
       </Loader>
     </div>
