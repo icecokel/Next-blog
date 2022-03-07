2f228b67e1468fdfb44804cb38aba1b14b114061
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-03-07 13:27:30 +0900

    modify mainpage

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,22 @@ import type { NextPage } from "next";
 import Link from "next/link";
 
 const Home: NextPage = () => {
+  const onClickSignUp = () => {
+    alert("Sorry, but the service is being prepared.");
+  };
   return (
     <div className="main-page">
       <h1>Welcome Eucalyptus </h1>
+
       <div>
         Sign up and make your own blog.
         <div>
-          <button>Sign Up</button>
+          <button onClick={onClickSignUp}>Sign Up</button>
         </div>
       </div>
+
       <div>
-        Popular Blog.
+        <p>Go to Popular Blog.</p>
         <ul>
           <Link href={"/testAdmin"}>
             <a>
