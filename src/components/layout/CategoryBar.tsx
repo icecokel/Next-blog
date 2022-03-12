c785e1c92e3ae4bded3257ef799de86b4058656d
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-03-12 11:06:10 +0900

    modify display logic

--- src/components/layout/CategoryBar.tsx
+++ src/components/layout/CategoryBar.tsx
@@ -9,25 +9,25 @@ const CategoryBar = () => {
   const category = useSelector((state: RootState) => state.category);
   const haveCategoryInfos = category.length !== 0;
 
-  if (!router.query.nickname) {
+  if (router.query.nickname || router.query.no) {
+    return (
+      <ul className="nav-bar">
+        <Loader isLoading={!haveCategoryInfos} size={5.5}>
+          {category.map((item) => {
+            return (
+              <Link href={"/category/" + item.categoryNo} key={item.categoryNo}>
+                <a>
+                  <li>{item.categoryName}</li>
+                </a>
+              </Link>
+            );
+          })}
+        </Loader>
+      </ul>
+    );
+  } else {
     return <div></div>;
   }
-
-  return (
-    <ul className="nav-bar">
-      <Loader isLoading={!haveCategoryInfos} size={5.5}>
-        {category.map((item) => {
-          return (
-            <Link href={"/category/" + item.categoryNo} key={item.categoryNo}>
-              <a>
-                <li>{item.categoryName}</li>
-              </a>
-            </Link>
-          );
-        })}
-      </Loader>
-    </ul>
-  );
 };
 
 export default CategoryBar;
