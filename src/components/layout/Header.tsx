7a1107426455fd21d8bd3a42b2bab80b6b0f2b17
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-03-12 11:06:51 +0900

    change userinfo box

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,9 +1,10 @@
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../store/modules";
 import CategoryBar from "./CategoryBar";
 import { UserVO } from "../../../store/modules/user";
+import BaseModal from "../common/BaseModal";
 
 const Header = () => {
   const userInfo = useSelector((state: RootState) => state.user);
@@ -16,7 +17,7 @@ const Header = () => {
 
   return (
     <>
-      <header className={"wrap"}>
+      <header className={"header-wrap"}>
         <UserInfoBox userInfo={userInfo} />
         <div className={"logo"} onClick={onClickLogo}>
           Eucalyptus<i className="material-icons">spa</i>
@@ -29,17 +30,37 @@ const Header = () => {
 };
 
 const UserInfoBox = ({ userInfo }: { userInfo: UserVO }) => {
-  const UNKNOWN = "알 수 없음";
+  const [isOpenSignInModal, setIsOpenSignInModal] = useState<boolean>(false);
+  const UNKNOWN = "Sign in";
+
+  const onClickLogIn = () => {
+    if (userInfo.userNickName) {
+      return;
+    }
+    setIsOpenSignInModal(!isOpenSignInModal);
+  };
 
   return (
-    <div className="header-user-info">{userInfo.userNickName ?? UNKNOWN}</div>
+    <>
+      <div className="header-user-info" onClick={onClickLogIn}>
+        {userInfo.userNickName || UNKNOWN}
+      </div>
+      <BaseModal isOpen={isOpenSignInModal} setIsOpen={setIsOpenSignInModal}>
+        <div className="login-wrap">
+          <input type="email" placeholder="email@email.com" />
+          <input type="password" placeholder="PassWord1@3$" />
+          <button> Sign In</button>
+          <p> Social Sign In</p>
+          <button className="sns-google"> Google</button>
+        </div>
+      </BaseModal>
+    </>
   );
 };
 
 const SearchBox = () => {
   return (
     <div className={"search"}>
-      <span>Search</span>
       <div>
         <i className="material-icons">search</i>
       </div>
