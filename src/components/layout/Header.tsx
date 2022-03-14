100294d5daa49426439f7c0d7f07ea4fad57530b
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-03-14 10:23:18 +0900

    divide component

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,10 +1,10 @@
 import { useRouter } from "next/router";
-import React, { useState } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../store/modules";
 import CategoryBar from "./CategoryBar";
-import { UserVO } from "../../../store/modules/user";
-import BaseModal from "../common/BaseModal";
+import UserInfoBox from "./UserInfoBox";
+import SearchBox from "./SearchBox";
 
 const Header = () => {
   const userInfo = useSelector((state: RootState) => state.user);
@@ -18,53 +18,14 @@ const Header = () => {
   return (
     <>
       <header className={"header-wrap"}>
-        <UserInfoBox userInfo={userInfo} />
+        <UserInfoBox userNickName={userInfo.userNickName} />
         <div className={"logo"} onClick={onClickLogo}>
           Eucalyptus<i className="material-icons">spa</i>
         </div>
         <SearchBox />
       </header>
-      <CategoryBar></CategoryBar>
+      <CategoryBar />
     </>
-  );
-};
-
-const UserInfoBox = ({ userInfo }: { userInfo: UserVO }) => {
-  const [isOpenSignInModal, setIsOpenSignInModal] = useState<boolean>(false);
-  const UNKNOWN = "Sign in";
-
-  const onClickLogIn = () => {
-    if (userInfo.userNickName) {
-      return;
-    }
-    setIsOpenSignInModal(!isOpenSignInModal);
-  };
-
-  return (
-    <>
-      <div className="header-user-info" onClick={onClickLogIn}>
-        {userInfo.userNickName || UNKNOWN}
-      </div>
-      <BaseModal isOpen={isOpenSignInModal} setIsOpen={setIsOpenSignInModal}>
-        <div className="login-wrap">
-          <input type="email" placeholder="email@email.com" />
-          <input type="password" placeholder="PassWord1@3$" />
-          <button> Sign In</button>
-          <p> Social Sign In</p>
-          <button className="sns-google"> Google</button>
-        </div>
-      </BaseModal>
-    </>
-  );
-};
-
-const SearchBox = () => {
-  return (
-    <div className={"search"}>
-      <div>
-        <i className="material-icons">search</i>
-      </div>
-    </div>
   );
 };
 
