d311b2935b34bddcecd8eb22d50c6bfbb656ace4
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-03-15 10:31:25 +0900

    modify icon tag

--- src/components/layout/SearchBox.tsx
+++ src/components/layout/SearchBox.tsx
@@ -3,9 +3,7 @@ import React from "react";
 const SearchBox = () => {
   return (
     <div className={"search"}>
-      <div>
-        <i className="material-icons">search</i>
-      </div>
+      <i className="material-icons">search</i>
     </div>
   );
 };
