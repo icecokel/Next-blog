5a995dc8e61a015667ae1ced7313f2d9cc5c9b7a
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-03-20 03:37:56 +0900

    adding feature login

--- src/components/layout/UserInfoBox.tsx
+++ src/components/layout/UserInfoBox.tsx
@@ -1,11 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import ApiOptions from "../../common/ApiOptions";
+import useAxios from "../../common/hooks/useAxios";
 import BaseModal from "../common/BaseModal";
 
 const UserInfoBox = () => {
   const [isOpenSignInModal, setIsOpenSignInModal] = useState<boolean>(false);
-  const UNKNOWN = "Sign in";
+  const [formData, setFormData] = useState<any>();
+  const { isLoading, data, error } = useAxios(ApiOptions.login, formData);
+
   // 임시 코드
   const userNickName = false;
+
+  useEffect(() => {
+    if (data?.message === "Success") {
+      setUserInfo();
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLoading, formData]);
 
   const onClickLogIn = () => {
     if (userNickName) {
@@ -14,18 +26,30 @@ const UserInfoBox = () => {
     setIsOpenSignInModal(!isOpenSignInModal);
   };
 
+  const onClickSignIn = (e: any) => {
+    e.preventDefault();
+
+    const email = e.target["email"].value;
+    const password = e.target["password"].value;
+
+    setFormData({ email, password });
+  };
+
+  const setUserInfo = () => {};
+
   return (
     <div
       className={userNickName ? "header-user-info" : "header-user-info-login"}
-      onClick={onClickLogIn}
     >
-      {userNickName || UNKNOWN}
+      <label onClick={onClickLogIn}>{userNickName || "Sign in"}</label>
 
       <BaseModal isOpen={isOpenSignInModal} setIsOpen={setIsOpenSignInModal}>
         <div className="login-wrap">
-          <input type="email" placeholder="email@email.com" />
-          <input type="password" placeholder="PassWord1@3$" />
-          <button> Sign In</button>
+          <form onSubmit={onClickSignIn}>
+            <input type="email" placeholder="email@email.com" name="email" />
+            <input type="password" placeholder="PassWord1@3$" name="password" />
+            <button>Sign In</button>
+          </form>
           <p> Social Sign In</p>
           <button className="sns-google"> Google</button>
         </div>
