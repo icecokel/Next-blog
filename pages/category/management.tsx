a720351b0262e83cf50c7b17220162629c34d966
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-03-31 23:41:33 +0900

    added management html

--- pages/category/management.tsx
+++ pages/category/management.tsx
@@ -1,6 +1,22 @@
 import React from "react";
 const ManagementPage = () => {
-  return <div></div>;
+  return (
+    <div className="management-wrap">
+      <div className="management-menu">
+        <ul>
+          <li>글 쓰기</li>
+          <li>카테고리</li>
+          <li>글관리</li>
+          <li>통계</li>
+          <li>블로그 설정</li>
+        </ul>
+      </div>
+      <div className="management-content">
+        <h2>sdf</h2>
+        <hr />
+      </div>
+    </div>
+  );
 };
 
 export default ManagementPage;
