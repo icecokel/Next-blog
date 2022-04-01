089b0cc45acbf2078e6d2e829f9164c1f6119610
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-04-01 23:12:16 +0900

    design html

--- pages/category/management.tsx
+++ pages/category/management.tsx
@@ -1,19 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+
+const MENU_LIST = [
+  "글쓰기",
+  "카테고리 관리",
+  "게시글 관리",
+  "통계",
+  "블로그 설정",
+];
+
 const ManagementPage = () => {
+  const [currentMenu, setCurrentMenu] = useState<string>(MENU_LIST[0]);
+
+  const onClickMenu = (e: any) => {
+    const { id } = e.target;
+    const no = Number.parseInt((id as string).replace("memu_", ""));
+    setCurrentMenu(MENU_LIST[no]);
+  };
+
+  const getMenuContent = () => {
+    const menuIndex = MENU_LIST.findIndex((menu) => menu === currentMenu);
+
+    switch (menuIndex) {
+      case 0:
+        return <span>글쓰기 컴포넌트</span>;
+      case 1:
+        return <span>카테고리 컴포넌트</span>;
+      case 2:
+        return <span>게시글 컴포넌트</span>;
+      case 3:
+        return <span>통계 컴포넌트</span>;
+      case 4:
+        return <span>블로그 컴포넌트</span>;
+    }
+  };
+
   return (
     <div className="management-wrap">
       <div className="management-menu">
         <ul>
-          <li>글 쓰기</li>
-          <li>카테고리</li>
-          <li>글관리</li>
-          <li>통계</li>
-          <li>블로그 설정</li>
+          {MENU_LIST.map((menu, index) => {
+            return (
+              <li
+                key={"memu_" + index}
+                onClick={onClickMenu}
+                id={"memu_" + index}
+              >
+                {menu}
+              </li>
+            );
+          })}
         </ul>
       </div>
       <div className="management-content">
-        <h2>sdf</h2>
-        <hr />
+        <div>
+          <h2>{currentMenu}</h2>
+        </div>
+        <div>{getMenuContent()}</div>
       </div>
     </div>
   );
