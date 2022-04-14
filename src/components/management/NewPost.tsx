4d57fe9ecab24cc35590e9797771532717f3149d
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-04-14 10:14:37 +0900

    added date picker

--- src/components/management/NewPost.tsx
+++ src/components/management/NewPost.tsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
 import ReactQuill from "react-quill";
+import DatePicker from "react-datepicker";
 
 type formDataVo = {
   title: string;
   content: string;
+  postDate: Date;
 };
 
 const NewPost = () => {
   const [formData, setFormData] = useState<formDataVo>({
     title: "",
     content: "",
+    postDate: new Date(),
   });
 
   const onChange = (e: any) => {
@@ -17,6 +20,7 @@ const NewPost = () => {
 
     setFormData({ ...formData, [id]: value });
   };
+
   return (
     <div className="editor-wrap">
       <input
@@ -32,6 +36,16 @@ const NewPost = () => {
           setFormData({ ...formData, content });
         }}
       />
+      <div>
+        <label>발행일</label>
+        <DatePicker
+          selected={formData.postDate}
+          dateFormat={"yyyy년 MM월 dd일"}
+          onChange={(date: Date) => {
+            setFormData({ ...formData, postDate: date });
+          }}
+        />
+      </div>
       <div className="button-wrap">
         <button>임시 저장</button>
         <button>발행</button>
