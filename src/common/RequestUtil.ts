79018f83f056c70c423be849d23fec4b4ad0ff6b
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-04-15 00:07:58 +0900

    change custom hook to util

---
+++ src/common/RequestUtil.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const BASE_URL = "/api";
+
+const RequestUtil = async (option: any, params?: any) => {
+  const axiosBody = { ...option };
+  const headers = {};
+
+  if (!option.url) {
+    throw "Check HttpOption";
+  }
+
+  switch (option.method) {
+    case "GET":
+      axiosBody["params"] = params;
+    default:
+      axiosBody["data"] = params;
+  }
+
+  axiosBody["url"] = BASE_URL + option.url;
+
+  return await axios(axiosBody)
+    .then((res) => {
+      return { data: res.data, error: undefined };
+    })
+    .catch((error) => {
+      return { data: undefined, error: error };
+    });
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+};
+
+export default RequestUtil;
