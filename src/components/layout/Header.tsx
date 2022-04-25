58f5bab4f9b80b2d36b17d98ea7a7d600fb6af6f
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-04-26 00:04:35 +0900

    modift Link Address

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,26 +5,29 @@ import LoginIcon from "./LoginIcon";
 import SearchIcon from "./SearchIcon";
 
 const Header = () => {
-  const userInfo = useSelector((state: RootState) => state.user);
+  const { userNickName, userAuthority } = useSelector(
+    (state: RootState) => state.user
+  );
   const router = useRouter();
 
   const onClickLogo = () => {
-    router.push("/" + userInfo.userNickName);
+    router.push("/blog/" + userNickName);
   };
 
   const onClickSettings = () => {
-    router.push("/category/management");
+    router.push("/blog/" + userNickName + "/category/management");
   };
 
   return (
     <header className="header-wrap">
       <div className="logo" onClick={onClickLogo}>
-        Eucalyptus<i className="material-icons">spa</i>
+        {userNickName}
+        <i className="material-icons">spa</i>
       </div>
       <div className="icons-wrap">
         <LoginIcon />
         <SearchIcon />
-        {userInfo.userAuthority && (
+        {userAuthority && (
           <div className="header-icon">
             <i className="material-icons" onClick={onClickSettings}>
               settings
