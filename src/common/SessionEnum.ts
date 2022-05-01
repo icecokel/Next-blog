dc5c1a8966d0e019b49ef3aa2b8bbfa1dd948235
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-05-01 16:45:21 +0900

    midify sesseion code and add random code

--- src/common/SessionEnum.ts
+++ src/common/SessionEnum.ts
@@ -1,3 +1,18 @@
 export enum SessionEnum {
-  userInfo = "u",
+  userInfo = "aY29V9jxMw3RTKjB",
+  fomrData = "9LSMkIOAAuUk5H4u",
 }
+
+// 랜덤 문자열 목록
+/*
+8zC8fw0t95ul00Z4
+8GbMLhZgWB1LgkOD
+5SaFlrDBtskSvWie
+FRWEenpSMCxPDWyh
+rozFu8rhz8MMH2pX
+ugzVkRSDFfQxSvA2
+GOHx8tmFYSp0U96E
+598wC36COxhH9bZD
+Y6sIk4PJGWy3n7eF
+cswW8IpVGj0Dhv8D
+*/
