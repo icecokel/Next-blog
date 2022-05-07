75df4640601828ef0ceb675a02abda76d758cba0
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-05-08 00:58:54 +0900

    add image handler

--- src/components/management/NewPost.tsx
+++ src/components/management/NewPost.tsx
@@ -1,8 +1,8 @@
-import React, { useState, useMemo, useEffect } from "react";
+import React, { useState, useMemo, useEffect, useRef } from "react";
 import DatePicker from "react-datepicker";
-import dynamic from "next/dynamic";
 import SesstionUtil from "../../common/SessionUtil";
 import { SessionEnum } from "../../../src/common/SessionEnum";
+import ReactQuill, { Quill } from "react-quill";
 
 type formDataVo = {
   title: string;
@@ -10,19 +10,16 @@ type formDataVo = {
   postDate: Date;
 };
 
-const Quill = dynamic(import("react-quill"), {
-  ssr: false,
-});
-
 /**
- * 이미지 버튼 클릭
- * 파일 배열에 저장
- * 파일 url을 화면에 이미지로 렌더링
+ * 이미지 버튼 클릭  -  핸들러 추가
+ * 파일 배열에 저장  -  상태 저장
+ * 파일 url을 화면에 이미지로 렌더링  -  렌더링 성공
  * 발행 클릭시, file을 s3에 업로드
  * 업로드 후 바로 링크 저장
  * formData에서 교체
  * formData를 DB or FireBase에 저장
  *
+ * 임시 저장 기능
  *
  */
 
@@ -34,7 +31,7 @@ const NewPost = () => {
   });
 
   const [images, setImages] = useState<Array<File>>();
-  const [imageLinks, setImageLinks] = useState<Array<string>>();
+  const quillRef = useRef<any>();
 
   useEffect(() => {
     setFormDataFromSesstionData();
@@ -46,7 +43,39 @@ const NewPost = () => {
     setFormData({ ...formData, [id]: value });
   };
 
-  const imageHandler = () => {};
+  const imageHandler = () => {
+    const input = document.createElement("input");
+
+    input.setAttribute("type", "file");
+    input.setAttribute("accept", "image/*");
+    input.setAttribute("class", "display-none");
+    document.body.appendChild(input);
+
+    input.click();
+
+    input.onchange = () => {
+      if (!input.files) {
+        return;
+      }
+
+      const file = input.files[0];
+      const tempImages: Array<File> = [...(images ?? [])];
+
+      tempImages.push(file);
+
+      const imageSrc = window.URL.createObjectURL(file);
+
+      const range = quillRef.current.getEditorSelection();
+
+      const Image = Quill.import("formats/image");
+      Image.sanitize = (url: string) => url;
+      quillRef.current.getEditor().insertEmbed(range.index, "image", imageSrc);
+      quillRef.current.getEditor().setSelection(range.index + 1);
+
+      setImages(tempImages);
+      document.body.removeChild(input);
+    };
+  };
 
   const getSeestion = () => {
     const data = SesstionUtil.getSession(SessionEnum.fomrData);
@@ -91,8 +120,9 @@ const NewPost = () => {
             { indent: "+1" },
           ],
           ["link", "image"],
-          ["clean", "imageHandler"],
+          ["clean"],
         ],
+        handlers: { image: imageHandler },
       },
     }),
     []
@@ -106,7 +136,8 @@ const NewPost = () => {
         id="title"
         onChange={onChange}
       />
-      <Quill
+      <ReactQuill
+        ref={quillRef}
         theme="snow"
         value={formData.content}
         modules={getModules}
