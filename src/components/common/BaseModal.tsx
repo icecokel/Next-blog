eb4f41c2f8e5827da65831a2d3b312fb7c346e9e
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-05-18 21:59:09 +0900

    modify common modal

--- src/components/common/BaseModal.tsx
+++ src/components/common/BaseModal.tsx
@@ -3,13 +3,12 @@ import ReactModal from "react-modal";
 
 const customStyles = {
   content: {
-    top: "50%",
+    top: "25%",
     left: "50%",
     right: "auto",
     bottom: "auto",
     marginRight: "-50%",
     transform: "translate(-50%, -50%)",
-
     maxWidth: "800px",
     minWidth: "300px",
   },
