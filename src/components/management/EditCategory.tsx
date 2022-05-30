5838a8683a0486cfc2aec08cd067df3ab4ec0f8c
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-05-30 11:36:54 +0900

    add feature category move

--- src/components/management/EditCategory.tsx
+++ src/components/management/EditCategory.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { MouseEventHandler, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../store/modules";
 import { CategoryVO } from "../../../store/modules/category";
@@ -18,14 +18,38 @@ const EditCategory = () => {
     }
     setCategoryList(category);
   };
+
+  const onChangeItemIndex = (index: number, sign: "up" | "down") => {
+    const list = [...(categoryList ?? [])];
+
+    switch (sign) {
+      case "up":
+        list.splice(index - 1, 0, list[index]);
+        list.splice(index + 1, 1);
+        break;
+      case "down":
+        list.splice(index + 2, 0, list[index]);
+        list.splice(index, 1);
+        break;
+    }
+
+    setCategoryList(list);
+  };
+
   return (
     <div className="edit-category-wrap">
-      {category.map((item, index) => {
+      {categoryList!.map((item, index) => {
         return (
           <EditCategory.item
             key={"category_management_" + index}
             categoryNo={item.categoryNo}
             categoryName={item.categoryName}
+            moveUp={() => {
+              onChangeItemIndex(index, "up");
+            }}
+            moveDown={() => {
+              onChangeItemIndex(index, "down");
+            }}
           />
         );
       })}
@@ -43,16 +67,27 @@ export default EditCategory;
 interface ICategoryProps {
   categoryNo: string;
   categoryName: string;
+  moveUp: MouseEventHandler<HTMLElement>;
+  moveDown: MouseEventHandler<HTMLElement>;
 }
 
-EditCategory.item = ({ categoryNo, categoryName }: ICategoryProps) => {
+EditCategory.item = ({
+  categoryNo,
+  categoryName,
+  moveUp,
+  moveDown,
+}: ICategoryProps) => {
   return (
     <div className="edit-category-item">
       <span className="edit-category-no">{categoryNo}</span>
       <span className="edit-category-name">{categoryName}</span>
       <div contextMenu="edit-category-arrow-wrap">
-        <i className="material-icons">expand_less</i>
-        <i className="material-icons">expand_more</i>
+        <i className="material-icons" onClick={moveUp}>
+          expand_less
+        </i>
+        <i className="material-icons" onClick={moveDown}>
+          expand_more
+        </i>
       </div>
     </div>
   );
