71e6418362550659f7e90b0f59cfbeea34bcb38f
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-06-16 09:40:18 +0900

    adding search event

--- src/components/layout/SearchIcon.tsx
+++ src/components/layout/SearchIcon.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SearchIcon = () => {
+  const [kerword, setKeyword] = useState<string>("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setKeyword(value);
+  };
+  const handleClickSearch = () => {
+    console.log(kerword);
+  };
   return (
-    <div className="header-icon">
-      <i className="material-icons">search</i>
+    <div className="search-wrap">
+      <input type="text" value={kerword} onChange={handleChange} />
+      <i className="material-icons" onClick={handleClickSearch}>
+        search
+      </i>
     </div>
   );
 };
