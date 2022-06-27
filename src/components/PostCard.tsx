97e27bbd8558ad58e58ea2243c37cde6613dfe9f
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-06-27 12:12:14 +0900

    modify post component

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Image from "next/image";
+import { PostVO } from "../common/Model";
 
-const PostCard = (props: { item: any }) => {
+const PostCard = (props: { item: PostVO }) => {
   const { item } = props;
 
   return (
     <div>
       <Image
-        src={item.thumbnailPath}
+        src="/resources/images/dafault.png"
         alt={item.title}
         width={300}
         height={400}
@@ -15,7 +16,7 @@ const PostCard = (props: { item: any }) => {
       <div>
         <p>{item.title}</p>
         <div>
-          <span>{item.registerDate}</span>
+          <span>{item.registDate}</span>
         </div>
       </div>
     </div>
