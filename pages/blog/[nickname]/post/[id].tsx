687aa92bcd92b0c89520321e176a2272b3675a46
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-06-28 05:01:00 +0900

    Specify Type

--- pages/blog/[nickname]/post/[id].tsx
+++ pages/blog/[nickname]/post/[id].tsx
@@ -1,10 +1,19 @@
+import { useRouter } from "next/router";
 import React from "react";
+import { PostVO } from "../../../../src/common/Model";
 import PostCard from "../../../../src/components/PostCard";
 
 const Post = () => {
+  const router = useRouter();
+  const id = router.query.id;
+
+  const getPost = () => {
+    return {} as PostVO;
+  };
+
   return (
     <div>
-      <PostCard />
+      <PostCard {...getPost()} />
     </div>
   );
 };
