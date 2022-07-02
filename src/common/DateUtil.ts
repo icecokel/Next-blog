2e7deabac8bfaf9f21805178e40ed941e73a01fe
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-07-02 09:14:33 +0900

    modify DateUtil add func : format Date

--- src/common/DateUtil.ts
+++ src/common/DateUtil.ts
@@ -9,5 +9,23 @@ const getTimeList = (type: "hours" | "minutes") => {
   return timeList;
 };
 
+export const formatDateToString = (
+  date: Date,
+  format: string = "YYYY-MM-DD"
+) => {
+  return format
+    .replaceAll(/YYYY/gi, date.getFullYear().toString())
+    .replaceAll(/YY/gi, date.getFullYear().toString().slice(-2))
+    .replaceAll(/MM/g, date.getMonth().toString().padStart(2, "0"))
+    .replaceAll(/M/g, date.getMonth().toString())
+    .replaceAll(/DD/gi, date.getDate().toString().padStart(2, "0"))
+    .replaceAll(/D/gi, date.getDate().toString())
+    .replaceAll(/HH/gi, date.getHours().toString().padStart(2, "0"))
+    .replaceAll(/H/gi, date.getHours().toString())
+    .replaceAll(/mm/g, date.getMinutes().toString().padStart(2, "0"))
+    .replaceAll(/m/g, date.getMinutes().toString())
+    .replaceAll(/ss/gi, date.getSeconds().toString());
+};
+
 export const HOURS = getTimeList("hours");
 export const MINUTES = getTimeList("minutes");
