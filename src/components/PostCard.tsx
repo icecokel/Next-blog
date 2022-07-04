eb10bbb75517af9435e714c1892b675832b85c2a
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-07-04 23:15:19 +0900

    apply style

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -6,20 +6,25 @@ import { formatDateToString } from "../common/DateUtil";
 const PostCard = (props: PostVO) => {
   return (
     <div className="post-wrap">
-      <Image
-        src="/resources/images/dafault.png"
-        alt={props.title}
-        width={300}
-        height={400}
-      />
-      <div className="post-title">
-        <p>{props.title}</p>
-        <div>
-          <span>
-            {formatDateToString(props.registDate, "yyyy년 MM월 dd일 hh:mm")}
-          </span>
+      <div className="post-title-wrap">
+        <div className="post-image-wrap">
+          <Image
+            src="/resources/images/dafault.png"
+            alt={props.title}
+            width={300}
+            height={400}
+          />
+        </div>
+        <div className="post-title">
+          <p>{props.title}</p>
+          <div>
+            <span>
+              {formatDateToString(props.registDate, "yyyy년 MM월 dd일 hh:mm")}
+            </span>
+          </div>
         </div>
       </div>
+      <div></div>
     </div>
   );
 };
