15d04978cb7e2c2da31d716545326a612b8ad82a
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-07-09 23:23:33 +0900

    add Main Component

---
+++ src/components/Main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div className="blog-main-wrap">
+      <div className="thumbnail">
+        <img src={"/resources/images/test.jpg"} alt="thumnail" />
+      </div>
+    </div>
+  );
+};
+
+export default Main;
