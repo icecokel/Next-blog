0acb0c921d4c8f8ff752658eaa52c6b042fd77f9
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-07-14 13:19:36 +0900

    modify error handling logic

--- src/common/RequestUtil.ts
+++ src/common/RequestUtil.ts
@@ -1,9 +1,12 @@
 import axios from "axios";
+import { useDispatch } from "react-redux";
 import { ApiOptionVo } from "./ApiOptions";
+import { ErrorVO, setError } from "../../store/modules/clientState";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const RequestUtil = async (option: ApiOptionVo, params?: any) => {
+  const dispatch = useDispatch();
   const axiosBody = { ...option };
   const headers = {};
 
@@ -22,10 +25,15 @@ const RequestUtil = async (option: ApiOptionVo, params?: any) => {
 
   return await axios(axiosBody)
     .then((res) => {
-      return { data: res.data, error: undefined };
+      return { data: res.data };
     })
     .catch((error) => {
-      return { data: undefined, error: error };
+      const errorObj: ErrorVO = {
+        isShowing: true,
+        title: error.response.data.code,
+        error: error.response.data.message,
+      };
+      dispatch(setError(errorObj));
     });
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
