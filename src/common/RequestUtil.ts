61f9083abcf2585da338032af2987e3d18b59e2f
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-07-15 20:08:53 +0900

    rollback;

--- src/common/RequestUtil.ts
+++ src/common/RequestUtil.ts
@@ -1,12 +1,9 @@
 import axios from "axios";
-import { useDispatch } from "react-redux";
 import { ApiOptionVo } from "./ApiOptions";
-import { ErrorVO, setError } from "../../store/modules/clientState";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const RequestUtil = async (option: ApiOptionVo, params?: any) => {
-  const dispatch = useDispatch();
   const axiosBody = { ...option };
   const headers = {};
 
@@ -25,15 +22,10 @@ const RequestUtil = async (option: ApiOptionVo, params?: any) => {
 
   return await axios(axiosBody)
     .then((res) => {
-      return { data: res.data };
+      return { data: res.data, error: undefined };
     })
     .catch((error) => {
-      const errorObj: ErrorVO = {
-        isShowing: true,
-        title: error.response.data.code,
-        error: error.response.data.message,
-      };
-      dispatch(setError(errorObj));
+      return { data: undefined, error: error };
     });
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
