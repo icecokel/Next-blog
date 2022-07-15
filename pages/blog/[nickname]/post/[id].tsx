83d2feaa186d3be4f3afe5a835d35bd54f4a7ae9
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-07-16 01:40:01 +0900

    change redering type static -> ssr

--- pages/blog/[nickname]/post/[id].tsx
+++ pages/blog/[nickname]/post/[id].tsx
@@ -1,39 +1,35 @@
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
+import { useQuery } from "react-query";
 import ApiOptions from "../../../../src/common/ApiOptions";
 import { PostVO } from "../../../../src/common/Model";
 import RequestUtil from "../../../../src/common/RequestUtil";
 import Loader from "../../../../src/components/common/Loader";
 import PostCard from "../../../../src/components/PostCard";
 
-const Post = () => {
-  const [postInfo, setPostInfo] = useState<PostVO>();
-  const router = useRouter();
-  const id = router.query.id;
-
-  useEffect(() => {
-    getPost();
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [id]);
-
-  const getPost = async () => {
-    const {
-      data: { item },
-    } = await RequestUtil(ApiOptions.getPostInfo, id);
-    setPostInfo({
-      ...item.posts,
-      registDate: new Date(item.posts.registDate),
-    });
-  };
-
+const Post = ({ item }: { item: PostVO }) => {
   return (
     <div>
-      <Loader isLoading={!postInfo}>
-        {postInfo && <PostCard {...postInfo} />}
-      </Loader>
+      <Loader isLoading={!item}>{item && <PostCard {...item} />}</Loader>
     </div>
   );
 };
 
 export default Post;
+
+export async function getServerSideProps(context: any) {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const router = useRouter();
+  const id = router.query.id;
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { data } = useQuery<any>(
+    ["post", id],
+    async () => await RequestUtil(ApiOptions.getPostInfo, id)
+  );
+
+  return {
+    props: {
+      item: data,
+    },
+  };
+}
