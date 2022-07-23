bcee2eafba77f4145e26f5c2ddb5da7988761c7d
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-07-23 09:09:03 +0900

    add function return type

--- src/common/EncryptUtil.ts
+++ src/common/EncryptUtil.ts
@@ -3,11 +3,11 @@ import CryptoJS from "crypto-js";
 const SECRET_KEY = process.env.NEXT_PUBLIC_SECRET_KET ?? "";
 
 const CryptoUtil = {
-  encrypt: (data: Object | JSON) => {
+  encrypt: (data: Object | JSON): string => {
     return CryptoJS.AES.encrypt(JSON.stringify(data), SECRET_KEY).toString();
   },
 
-  decrypt: (ciphertext: string) => {
+  decrypt: (ciphertext: string): string => {
     const bytes = CryptoJS.AES.decrypt(ciphertext, SECRET_KEY);
     return JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
   },
