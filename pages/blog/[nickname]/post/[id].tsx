2fb6450c2a19b4e853f4cd152f5eb473b88fa08e
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-07-26 11:15:31 +0900

    renaming

--- pages/blog/[nickname]/post/[id].tsx
+++ pages/blog/[nickname]/post/[id].tsx
@@ -5,17 +5,17 @@ import ApiOptions from "../../../../src/common/ApiOptions";
 import RequestUtil from "../../../../src/common/RequestUtil";
 import PostCard from "../../../../src/components/PostCard";
 
-const Post = ({ id }: { id: string; item: any }) => {
+const PostPage = ({ id }: { id: string; item: any }) => {
   const { data } = useQuery(
     ["post", id],
     async () => await RequestUtil(ApiOptions.getPostInfo, id)
   );
   const postInfo = data?.data?.item?.posts;
 
-  return <div>{postInfo && <PostCard {...postInfo} />}</div>;
+  return <>{postInfo && <PostCard {...postInfo} />}</>;
 };
 
-export default Post;
+export default PostPage;
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const id = context.query.id;
