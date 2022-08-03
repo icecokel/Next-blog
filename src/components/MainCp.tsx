6ca94e8dadcd8c1f9ecdec65419710e346a0cd82
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-08-03 22:09:14 +0900

    modify html for Semantics

--- src/components/MainCp.tsx
+++ src/components/MainCp.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const MainCp = () => {
   return (
-    <div className="blog-main-wrap">
+    <article className="blog-main-wrap">
       <div className="thumbnail">
         <img src={"/resources/images/test.jpg"} alt="thumnail" />
       </div>
-    </div>
+    </article>
   );
 };
 
