3b68426fa307c88dfcabb58b779f6d848fc4286d
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-08-16 18:03:49 +0900

    Apply VAC Patterns

--- pages/blog/[nickname]/index.tsx
+++ pages/blog/[nickname]/index.tsx
@@ -1,72 +1,8 @@
 import type { NextPage } from "next";
-import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
-import { setBlog } from "../../../store/modules/blog";
-import { setCategory } from "../../../store/modules/category";
-import { setUser } from "../../../store/modules/user";
-import RequestUtil from "../../../src/common/RequestUtil";
-import { getBlogInfo } from "../../../src/common/ApiOptions";
-import { useRouter } from "next/router";
-import { UserVO } from "../../../store/modules/user";
-import SessionUtil from "../../../src/common/SessionUtil";
-import { SessionEnum } from "../../../src/common/SessionEnum";
-import MainCp from "../../../src/components/MainCp";
-import { useQuery } from "react-query";
-import Loader from "../../../src/components/common/Loader";
+import MainCt from "../../../src/components/containers/MainCt";
 
 const BlogMainPage: NextPage = () => {
-  const router = useRouter();
-  const nickName = router.query.nickname;
-  const dispatch = useDispatch();
-  const [currentUser, setCurrentUser] = useState<UserVO>();
-  const { data, error, isLoading } = useQuery<any>(
-    "result",
-    async () => await RequestUtil(getBlogInfo, { nickname: nickName })
-  );
-
-  useEffect(() => {
-    setRedux();
-    if (!currentUser) {
-      getSessionUser();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [nickName, data]);
-
-  const getSessionUser = () => {
-    const data = SessionUtil.getSession(SessionEnum.userInfo);
-    if (data) {
-      setCurrentUser(data);
-    }
-  };
-
-  const setRedux = () => {
-    if (!data) {
-      return;
-    }
-    dispatch(setBlog(data.data.item.blogInfo));
-    dispatch(
-      setUser({
-        userNo: data.data.item.userInfo.userNo,
-        email: data.data.item.userInfo.email,
-        userName: "",
-        userEnglishName: "",
-        status: "",
-        userAuthority: currentUser?.email === data.data.item.userInfo.email,
-        userNickName: data.data.item.userInfo.nickName,
-      })
-    );
-    dispatch(setCategory(data.data.item.categorys));
-  };
-
-  if (error) {
-    <div></div>;
-  }
-
-  return (
-    <Loader isLoading={isLoading}>
-      <MainCp />
-    </Loader>
-  );
+  return <MainCt />;
 };
 
 export default BlogMainPage;
