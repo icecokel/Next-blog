ba20088259947ce88dd4e0cff022bfe9fa010686
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-08-31 06:50:14 +0900

    apply style

--- src/components/common/RequireLabel.tsx
+++ src/components/common/RequireLabel.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import styles from "../../../styles/require.module.scss";
+
 interface IRequireProps {
   isShowing?: boolean;
 }
 
 const RequireLabel = ({ isShowing = true }: IRequireProps) => {
-  return <div className={isShowing ? "require-label" : ""}></div>;
+  return <div className={isShowing ? styles.requireLabel : ""}></div>;
 };
 
 export default RequireLabel;
