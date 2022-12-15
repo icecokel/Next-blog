5114533e23c5882f563cf41ae1dcb7ef3259bc8a
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-12-15 17:38:46 +0900

    change index page because I won't use it

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,12 @@
 import type { NextPage } from "next";
-import Link from "next/link";
+import { useEffect } from "react";
 
 const Home: NextPage = () => {
-  const handleClickSignUp = () => {
-    alert("Sorry, but the service is being prepared.");
-  };
-  return (
-    <div className="entry-page-wrap">
-      <h1>Welcome Eucalyptus </h1>
-      <div>
-        Sign up and make your own blog.
-        <div>
-          <button onClick={handleClickSignUp}>Sign Up</button>
-        </div>
-      </div>
+  useEffect(() => {
+    window.location.replace(window.location.search + "/blog/solo");
+  }, []);
 
-      <div>
-        <p>Go to Popular Blog.</p>
-        <ul>
-          <Link href={"/blog/testUser"}>
-            <a>
-              <li>Test Admin</li>
-            </a>
-          </Link>
-        </ul>
-      </div>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Home;
