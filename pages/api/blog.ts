6e61c8204eec926c97b0fd87fc27ce631f656b10
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-12-17 20:41:15 +0900

    add Dynamodb

--- pages/api/blog.ts
+++ pages/api/blog.ts
@@ -1,76 +1,34 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { DynamoDBClient, GetItemCommand } from "@aws-sdk/client-dynamodb";
 
-export default function handler(
+const REGION = "ap-northeast-2";
+
+const client = new DynamoDBClient({
+  region: "ap-northeast-1",
+  credentials: {
+    accessKeyId: "AKIARKT7E4LVZX6Z6RUJ",
+    secretAccessKey: "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",
+  },
+});
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  res.status(200).json({
-    status: "Ok",
-    message: "Success",
-    item: {
-      userInfo: {
-        userNo: "",
-        nickName: "testUser",
-        email: "test@test.com",
+  const response = await client.send(
+    new GetItemCommand({
+      TableName: "USERS",
+      Key: {
+        id: {
+          S: "o7co1WlIUpTB2vHADT9V3e6e03NtsJzl",
+        },
+        email: {
+          S: "red9runge@gmail.com",
+        },
       },
-      blogInfo: {
-        blogNo: "9999",
-        blogName: "testUser의 블로그입니다",
-        blogDescription: "testUser의 블로그입니다",
-        favicomPath: "",
-      },
-      categorys: [
-        {
-          categoryNo: "0",
-          categoryName: "First Category",
-          groupNo: "",
-          groupOrder: "",
-          depth: "",
-        },
-        {
-          categoryNo: "1",
-          categoryName: "두번째 카테고리",
-          groupNo: "",
-          groupOrder: "",
-          depth: "",
-        },
-        {
-          categoryNo: "2",
-          categoryName: "세번째 Category",
-          groupNo: "",
-          groupOrder: "",
-          depth: "",
-        },
-        {
-          categoryNo: "3",
-          categoryName: "네번째 Category",
-          groupNo: "",
-          groupOrder: "",
-          depth: "",
-        },
-        {
-          categoryNo: "4",
-          categoryName: "다섯번째 Category",
-          groupNo: "",
-          groupOrder: "",
-          depth: "",
-        },
-        {
-          categoryNo: "5",
-          categoryName: "여섯번째 Category",
-          groupNo: "",
-          groupOrder: "",
-          depth: "",
-        },
-        {
-          categoryNo: "7",
-          categoryName: "일곱번째 Category",
-          groupNo: "",
-          groupOrder: "",
-          depth: "",
-        },
-      ],
-    },
-  });
+    })
+  );
+
+  return res.status(200).json(response.Item);
 }
