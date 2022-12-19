a0cf43ce904c4bad59c33b2fe679b5265a7d6992
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-12-19 23:43:28 +0900

    Apply Style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import "../styles/globals.scss";
 import "react-datepicker/dist/react-datepicker.css";
 import Header from "../src/components/layout/Header";
 import Footer from "../src/components/layout/Footer";
-import "react-quill/dist/quill.snow.css";
 import { wrapper } from "../store";
 import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
--- src/components/MainCp.tsx
+++ src/components/MainCp.tsx
@@ -1,17 +1,65 @@
-import React from "react";
 import styles from "./MainCp.module.scss";
+import { useState } from "react";
 
 const MainCp = () => {
+  const [tab, setTab] = useState<"posts" | "inst">("posts");
+
+  const handelClickPostsTabs = () => {
+    setTab("posts");
+  };
+  const handelClickInstTabs = () => {
+    setTab("inst");
+  };
   return (
     <article className={styles.mainWrap}>
-      <div className={styles.thumbnail}>
-        <img src={"/resources/images/test.jpg"} alt="thumnail" />
+      <div className={styles.profile}>
+        <h3>프로필</h3>
+        <div>사진</div>
+        <div>
+          <p>이름</p>
+          <p>간단 소개</p>
+        </div>
       </div>
-      <div className={styles.recentList}>
-        <p>최근 게시한 글 (30)</p>
-      </div>
+      <section className={styles.tabs}>
+        <ul>
+          <li
+            className={tab === "posts" ? styles.activedTab : ""}
+            onClick={handelClickPostsTabs}
+          >
+            글
+          </li>
+          <li
+            className={tab === "inst" ? styles.activedTab : ""}
+            onClick={handelClickInstTabs}
+          >
+            소개
+          </li>
+        </ul>
+      </section>
+      {tab === "posts" ? <MainCp.posts /> : <MainCp.inst />}
     </article>
   );
 };
 
 export default MainCp;
+
+MainCp.posts = () => {
+  return (
+    <section className={styles.recentList}>
+      <div>
+        <h3>최근 게시한 글 (30)</h3>
+      </div>
+      <div>
+        <h3>인기 글 (30)</h3>
+      </div>
+    </section>
+  );
+};
+
+MainCp.inst = () => {
+  return (
+    <div className={styles.profileDetail}>
+      <h3>소개</h3>
+    </div>
+  );
+};
