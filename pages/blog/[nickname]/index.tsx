11b1ec45a53b6fbc043521c7eff13937b8f66165
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-12-20 22:37:58 +0900

    apply dynamoDb

--- pages/blog/[nickname]/index.tsx
+++ pages/blog/[nickname]/index.tsx
@@ -1,26 +1,17 @@
 import type { NextPage } from "next";
 import MainCt from "../../../src/components/containers/MainCt";
-import dbClient from "../../../src/common/DynamoDbUtil";
-import { GetItemCommand } from "@aws-sdk/client-dynamodb";
+import { getItem, unmarshallByItem } from "../../../src/common/DynamoDbUtil";
 
 export async function getServerSideProps(context: any) {
-  const response = await dbClient.send(
-    new GetItemCommand({
-      TableName: "USERS",
-      Key: {
-        id: {
-          S: "o7co1WlIUpTB2vHADT9V3e6e03NtsJzl",
-        },
-        email: {
-          S: "red9runge@gmail.com",
-        },
-      },
-    })
-  );
+  const blogs = await getItem("BLOG", {
+    name: {
+      S: "leemon",
+    },
+  });
 
   return {
     props: {
-      data: response.Item,
+      blog: unmarshallByItem(blogs.Item),
     },
   };
 }
--- src/common/DynamoDbUtil.ts
+++ src/common/DynamoDbUtil.ts
@@ -1,7 +1,8 @@
 import { DynamoDBClient, GetItemCommand } from "@aws-sdk/client-dynamodb";
+import { unmarshall } from "@aws-sdk/util-dynamodb";
 
-const client = new DynamoDBClient({
-  region: process.env.REGION,
+export const client = new DynamoDBClient({
+  region: process.env.NEXT_PUBLIC_AWS_S3_REGION,
   credentials: {
     accessKeyId:
       process.env.NEXT_PUBLIC_AWS_S3_CREDENTTIALS_ACCESS_KEY_ID ?? "",
@@ -10,4 +11,18 @@ const client = new DynamoDBClient({
   },
 });
 
-export default client;
+export const getItem = (tableName: string, key: any) => {
+  return client.send(
+    new GetItemCommand({
+      TableName: tableName,
+      Key: key,
+    })
+  );
+};
+
+export const unmarshallByItem = (item: any) => {
+  if (!item) {
+    throw "Not Found";
+  }
+  return unmarshall(item);
+};
