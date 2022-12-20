fb3282ae382c1a8bf3bee73c8240808caa23a112
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-12-20 19:22:57 +0900

    apply dynamDb logic for test

--- pages/blog/[nickname]/index.tsx
+++ pages/blog/[nickname]/index.tsx
@@ -1,7 +1,33 @@
 import type { NextPage } from "next";
 import MainCt from "../../../src/components/containers/MainCt";
+import dbClient from "../../../src/common/DynamoDbUtil";
+import { GetItemCommand } from "@aws-sdk/client-dynamodb";
 
-const BlogMainPage: NextPage = () => {
+export async function getServerSideProps(context: any) {
+  const response = await dbClient.send(
+    new GetItemCommand({
+      TableName: "USERS",
+      Key: {
+        id: {
+          S: "o7co1WlIUpTB2vHADT9V3e6e03NtsJzl",
+        },
+        email: {
+          S: "red9runge@gmail.com",
+        },
+      },
+    })
+  );
+
+  return {
+    props: {
+      data: response.Item,
+    },
+  };
+}
+
+const BlogMainPage: NextPage = (props: any) => {
+  console.log(props);
+
   return <MainCt />;
 };
 
