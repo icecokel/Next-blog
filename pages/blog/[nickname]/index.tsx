dd5fa9c5d882004fa2af2af4fa314995cda1d9d9
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-12-25 11:51:40 +0900

    apply feature save to redux context

--- pages/blog/[nickname]/index.tsx
+++ pages/blog/[nickname]/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import MainCt from "../../../src/components/containers/MainCt";
-import { getItem, unmarshallByItem } from "../../../src/common/DynamoDbUtil";
+import { getItem, unmarshallByItem, getCategorys } from "../../../src/common/DynamoDbUtil";
 
 export async function getServerSideProps(context: any) {
   const blogs = await getItem("BLOG", {
@@ -8,18 +8,22 @@ export async function getServerSideProps(context: any) {
       S: "leemon",
     },
   });
+  const blogItem = unmarshallByItem(blogs.Item);
 
+  const profiles = await getItem("USERS", { id: { S: blogItem.userId } });
+  const profileItem = unmarshallByItem(profiles.Item);
+  const categoryItem = await getCategorys(blogItem.id);
   return {
     props: {
-      blog: unmarshallByItem(blogs.Item),
+      blog: blogItem,
+      users: profileItem,
+      categorys: categoryItem,
     },
   };
 }
 
 const BlogMainPage: NextPage = (props: any) => {
-  console.log(props);
-
-  return <MainCt />;
+  return <MainCt {...props} />;
 };
 
 export default BlogMainPage;
