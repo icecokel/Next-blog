0311614d4b3f8bd4f58439a136ed40ab5d5752dc
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-12-30 13:32:48 +0900

    change side bar

--- src/components/layout/MenuIcon.tsx
+++ src/components/layout/MenuIcon.tsx
@@ -2,8 +2,11 @@ import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../store/modules";
 import { CategoryVO } from "../../../store/modules/category";
+import styles from "./MenuIcon.module.scss";
+import Link from "next/link";
 
 const MenuIcon = () => {
+  const user = useSelector((state: RootState) => state.user);
   const category = useSelector((state: RootState) => state.category);
   const [openMenu, setOpenMenu] = useState<boolean>(false);
 
@@ -18,6 +21,9 @@ const MenuIcon = () => {
       <div onClick={handleToggle}>
         <i className="material-icons">menu</i>
       </div>
+      {openMenu && (
+        <MenuIcon.content category={category} handleClick={handleToggle} nickname={user.nickname} />
+      )}
     </>
   );
 };
@@ -27,4 +33,27 @@ export default React.memo(MenuIcon);
 interface IMenuContentsProps {
   category: CategoryVO[];
   handleClick: (e: any) => void;
+  nickname: string;
 }
+
+MenuIcon.content = ({ category, handleClick, nickname }: IMenuContentsProps) => {
+  return (
+    <article className={styles.memuWarpper} onClick={handleClick}>
+      <div className={styles.menuList}>
+        <div className={styles.profile}>Solo님의 블로그</div>
+        <ul>
+          {category.map((item) => {
+            const linkUrl = `/blog/${nickname.trim()}/category/${item.name.trim()}`;
+            return (
+              <Link href={linkUrl} key={item.id}>
+                <a onClick={() => {}}>
+                  <li id={"category_id_" + item.name}>{item.name}</li>
+                </a>
+              </Link>
+            );
+          })}
+        </ul>
+      </div>
+    </article>
+  );
+};
