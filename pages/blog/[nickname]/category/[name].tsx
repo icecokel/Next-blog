0f430dde11f3c9c9bd50400c08931b4f1c12e9a7
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2022-12-31 13:24:35 +0900

    modify Architecture add TODO

--- pages/blog/[nickname]/category/[name].tsx
+++ pages/blog/[nickname]/category/[name].tsx
@@ -1,9 +1,18 @@
 import { NextPage } from "next";
+import { PostVO } from "../../../../src/common/Model";
 
 import CategoryCt from "../../../../src/components/containers/CategoryCt";
+import { CategoryVO } from "../../../../store/modules/category";
 
 const CategoryPage: NextPage = () => {
-  return <CategoryCt />;
+  // TODO SSR 적용
+  const category: CategoryVO = {
+    blogId: "",
+    id: "",
+    name: "",
+  };
+  const postList: PostVO[] = [];
+  return <CategoryCt category={category} postList={postList} />;
 };
 
 export default CategoryPage;
