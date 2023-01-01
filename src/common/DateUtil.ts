13531acf27576e503e0c6cb04aa888aa00db49b0
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2023-01-02 01:33:03 +0900

    fix month then return month + 1 value

--- src/common/DateUtil.ts
+++ src/common/DateUtil.ts
@@ -13,32 +13,17 @@ const converDateType = (target: Date | string): Date => {
   return typeof target === "string" ? new Date(target) : target;
 };
 
-export const formatDateToString = (
-  date: Date,
-  format: string = "YYYY-MM-DD"
-): string => {
+export const formatDateToString = (date: Date, format: string = "YYYY-MM-DD"): string => {
   return format
     .replaceAll(/YYYY/gi, converDateType(date).getFullYear().toString())
     .replaceAll(/YY/gi, converDateType(date).getFullYear().toString().slice(-2))
-    .replaceAll(
-      /MM/g,
-      converDateType(date).getMonth().toString().padStart(2, "0")
-    )
-    .replaceAll(/M/g, converDateType(date).getMonth().toString())
-    .replaceAll(
-      /DD/gi,
-      converDateType(date).getDate().toString().padStart(2, "0")
-    )
+    .replaceAll(/MM/g, (converDateType(date).getMonth() + 1).toString().padStart(2, "0"))
+    .replaceAll(/M/g, (converDateType(date).getMonth() + 1).toString())
+    .replaceAll(/DD/gi, converDateType(date).getDate().toString().padStart(2, "0"))
     .replaceAll(/D/gi, converDateType(date).getDate().toString())
-    .replaceAll(
-      /HH/gi,
-      converDateType(date).getHours().toString().padStart(2, "0")
-    )
+    .replaceAll(/HH/gi, converDateType(date).getHours().toString().padStart(2, "0"))
     .replaceAll(/H/gi, converDateType(date).getHours().toString())
-    .replaceAll(
-      /mm/g,
-      converDateType(date).getMinutes().toString().padStart(2, "0")
-    )
+    .replaceAll(/mm/g, converDateType(date).getMinutes().toString().padStart(2, "0"))
     .replaceAll(/m/g, converDateType(date).getMinutes().toString())
     .replaceAll(/ss/gi, converDateType(date).getSeconds().toString());
 };
