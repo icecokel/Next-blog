9517010f16506850d5e57f94d31366ef40cf5125
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2023-01-05 02:11:09 +0900

    change postcard design to simple

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,22 +1,16 @@
 import React from "react";
-import Image from "next/image";
 import { PostVO } from "../common/Model";
-import { formatDateToString } from "../common/DateUtil";
+import { fromNow } from "../common/DateUtil";
 import styles from "./PostCard.module.scss";
 
 const PostCard = ({ title, registDate, contents }: PostVO) => {
   return (
     <article className={styles.wrapper}>
       <div className={styles.titleWarpper}>
-        <div className={styles.imageWarpper}>
-          <Image src="/resources/images/dafault.png" alt={title} width={800} height={300} />
-        </div>
         <div className={styles.postTitle}>
-          <p>{title}</p>
-          <div>
-            <span>{formatDateToString(new Date(registDate), "yyyy년 MM월 dd일 hh:mm")}</span>
-          </div>
+          <h1>{title}</h1>
         </div>
+        <div className={styles.postDetailInfo}>{fromNow(new Date(registDate))}</div>
       </div>
       <div className={styles.contentsWrapper}>
         <div className={styles.contents} dangerouslySetInnerHTML={{ __html: contents }} />
