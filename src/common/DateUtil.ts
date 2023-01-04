7c98c092b0c9beefa347eb407bde8a9b81267e9d
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2023-01-05 02:09:05 +0900

    apply moment change feature

--- src/common/DateUtil.ts
+++ src/common/DateUtil.ts
@@ -1,3 +1,6 @@
+import moment from "moment";
+import "moment/locale/ko";
+
 const _getTimeList = (type: "hours" | "minutes"): number[] => {
   const maxCount = type === "hours" ? 24 : 60;
   const timeList = new Array();
@@ -9,27 +12,12 @@ const _getTimeList = (type: "hours" | "minutes"): number[] => {
   return timeList;
 };
 
-const _converDateType = (target: Date | string): Date => {
-  return typeof target === "string" ? new Date(target) : target;
-};
-
-const _format = (date: Date, format: string = "YYYY-MM-DD"): string => {
-  return format
-    .replaceAll(/YYYY/gi, date.getFullYear().toString())
-    .replaceAll(/YY/gi, date.getFullYear().toString().slice(-2))
-    .replaceAll(/MM/g, (date.getMonth() + 1).toString().padStart(2, "0"))
-    .replaceAll(/M/g, (date.getMonth() + 1).toString())
-    .replaceAll(/DD/gi, date.getDate().toString().padStart(2, "0"))
-    .replaceAll(/D/gi, date.getDate().toString())
-    .replaceAll(/HH/gi, date.getHours().toString().padStart(2, "0"))
-    .replaceAll(/H/gi, date.getHours().toString())
-    .replaceAll(/mm/g, date.getMinutes().toString().padStart(2, "0"))
-    .replaceAll(/m/g, date.getMinutes().toString())
-    .replaceAll(/ss/gi, date.getSeconds().toString());
+export const fromNow = (date: Date) => {
+  return moment(date).fromNow();
 };
 
 export const formatDateToString = (date: Date | string, format: string = "YYYY-MM-DD"): string => {
-  return _format(_converDateType(date), format);
+  return moment(date).format(format);
 };
 
 export const HOURS = _getTimeList("hours");
