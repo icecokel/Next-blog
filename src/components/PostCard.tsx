562f824e1f2de4dc88c1c1d3ccc22ec12f8b7d6d
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2023-01-05 01:19:32 +0900

    modify code style

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -4,32 +4,22 @@ import { PostVO } from "../common/Model";
 import { formatDateToString } from "../common/DateUtil";
 import styles from "./PostCard.module.scss";
 
-const PostCard = (props: PostVO) => {
+const PostCard = ({ title, registDate, contents }: PostVO) => {
   return (
     <article className={styles.wrapper}>
       <div className={styles.titleWarpper}>
         <div className={styles.imageWarpper}>
-          <Image
-            src="/resources/images/dafault.png"
-            alt={props.title}
-            width={800}
-            height={300}
-          />
+          <Image src="/resources/images/dafault.png" alt={title} width={800} height={300} />
         </div>
         <div className={styles.postTitle}>
-          <p>{props.title}</p>
+          <p>{title}</p>
           <div>
-            <span>
-              {formatDateToString(props.registDate, "yyyy년 MM월 dd일 hh:mm")}
-            </span>
+            <span>{formatDateToString(new Date(registDate), "yyyy년 MM월 dd일 hh:mm")}</span>
           </div>
         </div>
       </div>
       <div className={styles.contentsWrapper}>
-        <div
-          className={styles.contents}
-          dangerouslySetInnerHTML={{ __html: props.contents }}
-        />
+        <div className={styles.contents} dangerouslySetInnerHTML={{ __html: contents }} />
       </div>
     </article>
   );
