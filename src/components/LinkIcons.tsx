fe09e2ec9c2710b84d47670511022dddc67a62f4
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2023-01-06 21:01:54 +0900

    added component for Links

---
+++ src/components/LinkIcons.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+interface IProps {
+  githubAddress?: string;
+}
+
+const LinkIcons = ({ githubAddress }: IProps) => {
+  const handleClickGitHub = () => {
+    window.open(githubAddress);
+  };
+  return (
+    <section>
+      {githubAddress && (
+        <img
+          onClick={handleClickGitHub}
+          src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+          alt="git"
+        />
+      )}
+    </section>
+  );
+};
+
+export default LinkIcons;
