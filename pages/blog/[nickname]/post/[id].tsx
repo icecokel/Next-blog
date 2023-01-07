68592f8ef398289e025dab9d49b8623c1be0867e
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2023-01-07 23:15:37 +0900

    change post card logic

--- pages/blog/[nickname]/post/[id].tsx
+++ pages/blog/[nickname]/post/[id].tsx
@@ -1,9 +1,33 @@
 import { GetServerSideProps } from "next";
 import React, { useEffect } from "react";
-import { getItem, unmarshallByItem } from "../../../../src/common/DynamoDbUtil";
+import { useDispatch } from "react-redux";
+import { getItem, unmarshallByItem, getCategorys } from "../../../../src/common/DynamoDbUtil";
 import PostCard from "../../../../src/components/PostCard";
+import { setBlog } from "../../../../store/modules/blog";
+import { setCategory } from "../../../../store/modules/category";
+import { setUser } from "../../../../store/modules/user";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
+  const blogs = await getItem("BLOG", {
+    name: {
+      S: context.query.nickname,
+    },
+  });
+
+  if (!blogs.Item) {
+    return {
+      props: {
+        code: 404,
+      },
+    };
+  }
+
+  const blogItem = unmarshallByItem(blogs.Item);
+
+  const profiles = await getItem("USERS", { id: { S: blogItem.userId } });
+  const profileItem = unmarshallByItem(profiles.Item);
+  const categoryItems = await getCategorys(blogItem.id);
+
   const post = await getItem("POSTS", { id: { S: context.query.id } });
 
   if (!post.Item) {
@@ -15,24 +39,33 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   }
 
   const postItem = unmarshallByItem(post.Item);
-
-  const profiles = await getItem("USERS", { id: { S: postItem.registId } });
-  const profileItem = unmarshallByItem(profiles.Item);
   return {
     props: {
+      blog: blogItem,
       post: postItem,
       user: profileItem,
+      categorys: categoryItems,
     },
   };
 };
 
-const PostPage = (props: any) => {
+const PostPage = ({ blog, post, user, categorys, code }: any) => {
+  const dispatch = useDispatch();
   useEffect(() => {
-    if (props.code === 404) {
+    if (code === 404) {
       window.location.replace(window.location.origin + "/404");
+    } else {
+      setRedux();
     }
   }, []);
-  return <PostCard {...props} />;
+
+  const setRedux = () => {
+    dispatch(setBlog(blog));
+    dispatch(setCategory(categorys));
+    dispatch(setUser(user));
+  };
+
+  return <PostCard post={post} user={user} />;
 };
 
 export default PostPage;
