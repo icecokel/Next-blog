d11e18fa95aff9971b4c28d5a1133d260569de93
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2023-01-18 07:53:24 +0900

    remove NewPostCp cuz It is an unnecessary process change NewPost for new editor lib

--- src/components/management/NewPost.tsx
+++ src/components/management/NewPost.tsx
@@ -1,16 +1,16 @@
+import dynamic from "next/dynamic";
 import React from "react";
 import DatePicker from "react-datepicker";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../../store/modules";
 import { setPost } from "../../../store/modules/post";
 import { HOURS, MINUTES } from "../../common/DateUtil";
-import BaseEditor, { IBaseEditorProps } from "../common/BaseEditor";
 
-const NewPost = ({
-  imageList,
-  setImageList,
-}: Pick<IBaseEditorProps, "imageList" | "setImageList">) => {
+const BaseEditor = dynamic(import("../common/BaseEditor"), { ssr: false });
+
+const NewPost = () => {
   const state = useSelector((state: RootState) => state.post);
+  const registDate = new Date(state.registDate);
   const dispatch = useDispatch();
 
   const handleChangeText = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -18,7 +18,11 @@ const NewPost = ({
     dispatch(setPost({ ...state, [name]: value }));
   };
 
-  const handleChangeContents = (value: any) => {
+  const handleClickPostButton = () => {
+    console.log(state);
+  };
+
+  const handleChangeContents = (value: string | undefined) => {
     dispatch(setPost({ ...state, contents: value }));
   };
   const handleChangeDatePicker = (date: any) => {
@@ -27,7 +31,6 @@ const NewPost = ({
   const handleChangeTime = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const { value, name } = e.target;
     const time = Number.parseInt(value);
-    const { registDate } = { ...state };
 
     if (name === "postTimehour") {
       registDate.setHours(time);
@@ -35,11 +38,11 @@ const NewPost = ({
       registDate.setMinutes(time);
     }
 
-    dispatch(setPost({ ...state, registDate: registDate }));
+    dispatch(setPost({ ...state, registDate: registDate.getTime() }));
   };
 
   return (
-    <>
+    <div className="editor-wrap">
       <input
         type="text"
         placeholder="제목을 입력해주세요"
@@ -47,28 +50,19 @@ const NewPost = ({
         onChange={handleChangeText}
         value={state.title}
       />
-      <BaseEditor
-        value={state.contents}
-        imageList={imageList}
-        setImageList={setImageList}
-        onChange={handleChangeContents}
-      />
+      <BaseEditor getEditorHTML={handleChangeContents} />
       <div>
         <p>발행일</p>
         <div className="editor-date-wrap">
           <div className="editor-date">
             <DatePicker
-              selected={state.registDate}
+              selected={new Date(state.registDate)}
               dateFormat={"yyyy년 MM월 dd일"}
               onChange={handleChangeDatePicker}
             />
           </div>
           <div className="editor-time">
-            <select
-              onChange={handleChangeTime}
-              name={"postTimehour"}
-              value={state.registDate.getHours()}
-            >
+            <select onChange={handleChangeTime} name={"postTimehour"} value={registDate.getHours()}>
               {HOURS.map((code) => {
                 return (
                   <option key={"hour_" + code} value={code}>
@@ -80,7 +74,7 @@ const NewPost = ({
             <select
               onChange={handleChangeTime}
               name={"postTimeMinute"}
-              value={state.registDate.getMinutes()}
+              value={registDate.getMinutes()}
             >
               {MINUTES.map((code) => {
                 return (
@@ -93,7 +87,10 @@ const NewPost = ({
           </div>
         </div>
       </div>
-    </>
+      <div className="button-wrap">
+        <button onClick={handleClickPostButton}>발행</button>
+      </div>
+    </div>
   );
 };
 
