76b8ec4df45431aa1f61da406623284105d1d8dd
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2023-01-24 20:39:42 +0900

    modify text

--- src/components/containers/MamagementCt.tsx
+++ src/components/containers/MamagementCt.tsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect, ReactElement } from "react";
 import { useRouter } from "next/router";
-import EditCategory from "../management/EditCategory";
+import EditMenu from "../management/EditMenu";
 import EditBlogInfo from "../management/EditBlogInfo";
 import ManagementCp from "../management/ManagementCp";
 import useAuth from "../../common/hooks/useAuth";
 import NewPost from "../management/NewPost";
 
-const MENU_LIST = ["글쓰기", "카테고리 관리", "블로그 설정"];
+const MENU_LIST = ["글쓰기", "메뉴 관리", "블로그 설정"];
 
 const MamagementCt = () => {
   const [currentMenu, setCurrentMenu] = useState<string>(MENU_LIST[0]);
@@ -32,7 +32,7 @@ const MamagementCt = () => {
         component = <NewPost />;
         break;
       case 1:
-        component = <EditCategory />;
+        component = <EditMenu />;
         break;
       case 2:
         component = <EditBlogInfo />;
