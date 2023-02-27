aab47f3620840d1dc8a06de8d024202144198e17
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2023-02-28 00:33:03 +0900

    added feature to apply favicon

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,14 +5,16 @@ import MenuIcon from "./MenuIcon";
 import styles from "./Header.module.scss";
 import LoginIcon from "./LoginIcon";
 import useAuth from "../../common/hooks/useAuth";
+import Head from "next/head";
 
 const Header = () => {
   const { nickname } = useSelector((state: RootState) => state.user);
+  const { faviconPath } = useSelector((state: RootState) => state.blog);
   const { isOwner } = useAuth();
   const router = useRouter();
 
   const handleClickLogo = () => {
-    router.push(`${nickname ? `/blog/"${nickname}` : "/"}`);
+    router.push(`${nickname ? `/blog/${nickname}` : "/"}`);
   };
 
   const handleClickSettings = () => {
@@ -20,25 +22,30 @@ const Header = () => {
   };
 
   return (
-    <header className={styles.wrapper}>
-      <article className={styles.headerWrapper}>
-        <div className={styles.logo} onClick={handleClickLogo}>
-          {nickname}
-          <span className="material-icons">spa</span>
-        </div>
-        <div className={styles.iconsWrapper}>
-          <LoginIcon />
-          <MenuIcon />
-          {isOwner && (
-            <div className={styles.icon}>
-              <span className="material-icons" onClick={handleClickSettings}>
-                settings
-              </span>
-            </div>
-          )}
-        </div>
-      </article>
-    </header>
+    <>
+      <Head>
+        <link rel="shortcut icon" href={faviconPath} />
+      </Head>
+      <header className={styles.wrapper}>
+        <article className={styles.headerWrapper}>
+          <div className={styles.logo} onClick={handleClickLogo}>
+            {nickname}
+            <span className="material-icons">spa</span>
+          </div>
+          <div className={styles.iconsWrapper}>
+            <LoginIcon />
+            <MenuIcon />
+            {isOwner && (
+              <div className={styles.icon}>
+                <span className="material-icons" onClick={handleClickSettings}>
+                  settings
+                </span>
+              </div>
+            )}
+          </div>
+        </article>
+      </header>
+    </>
   );
 };
 
