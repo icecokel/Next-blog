bfa3f5bf0f2f06a3b2d71f24641ef46d9282e7a5
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2023-03-04 01:08:08 +0900

    change clientState for modal cuz added common modal

--- store/modules/clientState.ts
+++ store/modules/clientState.ts
@@ -7,23 +7,38 @@
  */
 
 // 액션 타입 선언
-export const SET_ERROR = "clientState/SET_ERROR" as const;
+export const SET_ERROR_MODAL = "clientState/SET_ERROR_MODAL" as const;
+export const SET_COMMON_MODAL = "clientState/SET_COMMON_MODAL" as const;
 
 // 액션 함수 선언
-export const setError = (error: ErrorVO) => ({
-  type: SET_ERROR,
+export const setErrorModal = (error: ErrorModalVO) => ({
+  type: SET_ERROR_MODAL,
   payload: error,
+});
+// 액션 함수 선언
+export const setCommonModal = (common: CommonModalVO) => ({
+  type: SET_COMMON_MODAL,
+  payload: common,
 });
 
 // 액센 객체들에 대한 타입 선언
-export type ClientStateAction = ReturnType<typeof setError>;
+export type ClientStateAction = ReturnType<typeof setErrorModal | typeof setCommonModal>;
 
 // 카테고리 타입
 export type ClientStateVO = {
-  error: ErrorVO;
+  modal: {
+    error: ErrorModalVO;
+    common: CommonModalVO;
+  };
 };
 
-export interface ErrorVO {
+interface CommonModalVO {
+  isShowing: boolean;
+  title: string;
+  text: string;
+}
+
+export interface ErrorModalVO {
   isShowing: boolean;
   title: string;
   error: string;
@@ -31,10 +46,17 @@ export interface ErrorVO {
 // 초기 값 선언
 // 배열로 선언
 const initialState: ClientStateVO = {
-  error: {
-    isShowing: false,
-    title: "",
-    error: "",
+  modal: {
+    error: {
+      isShowing: false,
+      title: "",
+      error: "",
+    },
+    common: {
+      isShowing: false,
+      title: "",
+      text: "",
+    },
   },
 };
 
@@ -42,10 +64,21 @@ const initialState: ClientStateVO = {
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (state: ClientStateVO = initialState, action: ClientStateAction) => {
   switch (action.type) {
-    case SET_ERROR:
+    case SET_ERROR_MODAL:
       return {
         ...state,
-        error: action.payload,
+        modal: {
+          ...state.modal,
+          error: action.payload,
+        },
+      };
+    case SET_COMMON_MODAL:
+      return {
+        ...state,
+        modal: {
+          ...state.modal,
+          common: action.payload,
+        },
       };
 
     default:
