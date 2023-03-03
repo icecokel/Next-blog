2e218c9e844a4994f279720f8128264be017c961
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2023-03-04 01:07:08 +0900

    change date lib moment to luxon

--- src/common/util/DateUtil.ts
+++ src/common/util/DateUtil.ts
@@ -1,16 +1,16 @@
-import moment from "moment";
-import "moment/locale/ko";
+import { DateTime } from "luxon";
 
 const _getTimeList = (type: "hours" | "minutes"): number[] => {
   const maxCount = type === "hours" ? 24 : 60;
   return Array.from({ length: maxCount }, (v, i) => i);
 };
 export const fromNow = (date: Date) => {
-  return moment(date).fromNow();
+  return DateTime.fromJSDate(date).toRelative();
 };
 
-export const formatDateToString = (date: Date | string, format: string = "YYYY-MM-DD"): string => {
-  return moment(date).format(format);
+export const formatDateToString = (date: Date | string, format: string = "yyyy-LL-dd"): string => {
+  const dateByType = typeof date === "string" ? DateTime.fromISO(date) : DateTime.fromJSDate(date);
+  return dateByType.toFormat(format);
 };
 
 export const HOURS = _getTimeList("hours");
