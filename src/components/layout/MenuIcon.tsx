181d544782d3cd5731f5c99fff14026c471828d2
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2023-03-05 17:47:10 +0900

    Change component structure

--- src/components/layout/MenuIcon.tsx
+++ src/components/layout/MenuIcon.tsx
@@ -1,70 +1,23 @@
-import React, { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
-import { RootState } from "../../../store/modules";
-import { MenuVO } from "../../../store/modules/menu";
-import styles from "./MenuIcon.module.scss";
-import Link from "next/link";
-import { sortByKey } from "../../common/util/ArrayUtil";
+import React, { useState } from "react";
+import SideBar from "./SideBar";
 
 const MenuIcon = () => {
-  const user = useSelector((state: RootState) => state.user);
-  const menu = useSelector((state: RootState) => state.menu);
   const [openMenu, setOpenMenu] = useState<boolean>(false);
 
-  const handleClickOther = ({ target: { tagName } }: any) => {
-    if (tagName === "ARTICLE" || tagName === "SPAN") {
-      setOpenMenu(!openMenu);
-    }
+  const handleToggleOpen = () => {
+    setOpenMenu(!openMenu);
   };
-
-  useEffect(() => {
-    const handleEscape = ({ key }: any) => {
-      if (key === "Escape") {
-        setOpenMenu(false);
-      }
-    };
-    window.addEventListener("keydown", handleEscape, false);
-    return window.removeEventListener("keydown", handleEscape, false);
-  }, []);
 
   return (
     <>
-      <div onClick={handleClickOther}>
+      <div onClick={handleToggleOpen}>
         <span className="material-icons">menu</span>
       </div>
-      {openMenu && (
-        <MenuIcon.content menu={menu} handleClick={handleClickOther} nickname={user.nickname} />
-      )}
+      {openMenu && <MenuIcon.content handleToggle={handleToggleOpen} />}
     </>
   );
 };
 
 export default React.memo(MenuIcon);
 
-interface IMenuContentsProps {
-  menu: MenuVO[];
-  handleClick: (e: any) => void;
-  nickname: string;
-}
-
-MenuIcon.content = ({ menu, handleClick, nickname }: IMenuContentsProps) => {
-  const sortedMenu = sortByKey(menu, "index");
-
-  return (
-    <article className={styles.memuWarpper} onClick={handleClick}>
-      <div className={styles.menuList}>
-        <div className={styles.profile}>{nickname}님의 블로그</div>
-        <ul>
-          {sortedMenu.map((item) => {
-            const encodedUri = `/blog/${nickname.trim()}/m/${encodeURIComponent(item.name.trim())}`;
-            return (
-              <Link href={encodedUri} key={item.id}>
-                <li>{item.name}</li>
-              </Link>
-            );
-          })}
-        </ul>
-      </div>
-    </article>
-  );
-};
+MenuIcon.content = SideBar;
