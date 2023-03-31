3dc8d4f4ab131e374a6ef578b66915bf6dcf6141
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2023-04-01 00:05:59 +0900

    modify fetch logic

--- pages/blog/[nickname]/m/[name].tsx
+++ pages/blog/[nickname]/m/[name].tsx
@@ -1,11 +1,10 @@
 import { GetServerSidePropsContext, NextPage } from "next";
 import { PostVO } from "../../../../src/common/constant/Model";
 import MenuCt from "../../../../src/components/containers/MenuCt";
-import { MenuVO } from "../../../../store/modules/menu";
 import useDispatchInitialization, {
   IInitializationProps,
 } from "../../../../src/common/hooks/useDispatchInitialization";
-import { searchData, getData } from "../../../../src/common/service/FireBaseService";
+import { getPosts } from "../../../api/menus/getPosts";
 
 export async function getServerSideProps({
   query: { nickname, name },
@@ -15,34 +14,19 @@ export async function getServerSideProps({
       notfound: true,
     };
   }
-  const blog = await getData("blog", nickname.toString());
-
-  if (!blog) {
-    return {
-      notfound: true,
-    };
-  }
-  const currentMenu = blog.menu.find((item: MenuVO) => item.name === name);
-
-  if (!currentMenu) {
-    return {
-      notfound: true,
-    };
-  }
-
-  const posts = await searchData("post", {
-    fieldPath: "menuId",
-    opStr: "==",
-    value: currentMenu.id,
+  const data = await getPosts({
+    name: name.toString(),
+    nickname: nickname.toString(),
+    page: 1,
+    rowCount: 3,
   });
-
+  if (!data) {
+    return {
+      notfound: true,
+    };
+  }
   return {
-    props: {
-      blog: blog,
-      users: blog.user,
-      menus: blog.menu,
-      posts: posts,
-    },
+    props: { ...data },
   };
 }
 
