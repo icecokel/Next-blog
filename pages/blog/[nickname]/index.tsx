f8d59d097e6eaf8aa0b7369cf82b755b09176e71
LEESANGMIN\LeeSangmin <red9runge@gmail.com>
2023-04-14 13:45:19 +0900

    apply serverSide dispatch

--- pages/blog/[nickname]/index.tsx
+++ pages/blog/[nickname]/index.tsx
@@ -1,34 +1,44 @@
-import type { GetServerSidePropsContext, NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import MainCt from "../../../src/components/containers/MainCt";
 import useDispatchInitialization, {
   IInitializationProps,
 } from "../../../src/common/hooks/useDispatchInitialization";
 import { getData } from "../../../src/common/service/FireBaseService";
+import { wrapper } from "../../../store";
+import { BlogVO, setBlog } from "../../../store/modules/blog";
+import { MenuVO, setMenu } from "../../../store/modules/menu";
+import { setUser, UserVO } from "../../../store/modules/user";
+import { ApiStatus } from "../../../src/common/constant/Enum";
 
-export async function getServerSideProps({ query: { nickname } }: GetServerSidePropsContext<any>) {
-  if (!nickname) {
-    return {
-      notFound: true,
-    };
-  }
-  const blog = await getData("blog", nickname.toString());
+export const getServerSideProps: GetServerSideProps = wrapper.getServerSideProps(
+  (store) =>
+    async ({ query: { nickname } }) => {
+      if (!nickname) {
+        return {
+          notFound: true,
+        };
+      }
+      const blog = await getData("blog", nickname.toString());
 
-  if (!blog) {
-    return {
-      notFound: true,
-    };
-  }
-  return {
-    props: {
-      blog: blog,
-      users: blog.user,
-      menus: blog.menu,
-    },
-  };
-}
+      if (!blog) {
+        return {
+          notFound: true,
+        };
+      }
+
+      store.dispatch(setBlog(blog as BlogVO));
+      store.dispatch(setMenu(blog.menu as MenuVO[]));
+      store.dispatch(setUser(blog.user as UserVO));
+
+      return {
+        props: {
+          status: ApiStatus.OK,
+        },
+      };
+    }
+);
 
 const BlogMainPage: NextPage<IInitializationProps> = (props) => {
-  useDispatchInitialization(props);
   return <MainCt />;
 };
 
